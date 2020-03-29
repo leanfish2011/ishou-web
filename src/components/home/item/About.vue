899b25a1b08f70388df2abeb0b8decb0ac4139ff
yangtzeyufei <yangtze_yufei@163.com>
2020-03-29 15:30:45 +0800

    feat(about):增加about内容

--- src/components/home/item/About.vue
+++ src/components/home/item/About.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="about">
     <nav-menu></nav-menu>
-    <div v-bind:style="{minHeight: Height+'px'}">
-      <p>关于</p>
+    <div v-bind:style="{minHeight: Height+'px'}" class="aboutMain">
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity, index) in activities"
+          :timestamp="activity.timestamp"
+          :key="index"
+          :icon="activity.icon"
+          :type="activity.type"
+          :color="activity.color"
+          size="large" placement="top">
+          <el-card>
+            <h4>{{activity.title}}</h4>
+            <p>{{activity.content}}</p>
+          </el-card>
+        </el-timeline-item>
+      </el-timeline>
     </div>
     <bottom-footer></bottom-footer>
   </div>
@@ -20,7 +34,31 @@
     },
     data() {
       return {
-        Height: 0
+        Height: 0,
+        activities: [
+          {
+            timestamp: '2020-03-29',
+            title: '爱收藏网站上线',
+            content: '之前的博客系统停用，打造用于收藏网站的系统。',
+            type: 'primary',
+            icon: 'el-icon-more'
+          },
+          {
+            timestamp: '2018-05-03',
+            title: '基于spring cloud博客系统',
+            content: '使用spring cloud重新实现了博客系统。拆分了各个服务，前后端分离',
+            color: '#0bbd87'
+          },
+          {
+            timestamp: '2018-02-09',
+            title: '多人博客系统上线',
+            content: '打造了可以多人注册使用的博客系统。',
+          },
+          {
+            timestamp: '2014-11-04',
+            title: '个人博客上线',
+            content: '基于免费的云服务器，上线个人博客系统。',
+          }]
       }
     },
     mounted() {
@@ -39,5 +77,10 @@
     background: lightyellow;
     height: 100%;
     width: 100%;
+  }
+
+  #aboutMain {
+    margin-top: 5px;
+    text-align: center;
   }
 </style>
