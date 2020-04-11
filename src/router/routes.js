b89b1c81f7085b5d0966e4ee4ddbfa32a40e971f
yangtzeyufei <yangtze_yufei@163.com>
2020-04-11 10:59:54 +0800

    feat(homesite):增加首页管理

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,7 @@ import Login from '../components/home/login/Login.vue'
 import Register from '../components/home/login/Register.vue'
 import Manage from '../components/manage/Manage.vue'
 import Site from '../components/manage/site/Site.vue'
+import HomeSite from '../components/manage/site/HomeSite.vue'
 import User from '../components/manage/user/User.vue'
 import Role from '../components/manage/user/Role.vue'
 import Table from '../components/test/elmentui/Table'
@@ -37,6 +38,7 @@ export default [
     component: Manage,
     children: [
       {path: "/site", component: Site},
+      {path: "/homesite", component: HomeSite},
       {path: "/user", component: User},
       {path: "/role", component: Role}
     ]
