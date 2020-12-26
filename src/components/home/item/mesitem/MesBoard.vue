2cecd4ca014350017f7a09ed89227086681028b9
yangtzeyufei <yangtze_yufei@163.com>
2020-12-26 23:00:46 +0800

    feat(message):增加留言板功能

--- src/components/home/item/mesitem/MesBoard.vue
+++ src/components/home/item/mesitem/MesBoard.vue
@@ -1,118 +1,24 @@
 <template>
-  <div class="mes-container">
-    <el-form ref="mesForm" :model="mesModel" label-width="80px" :rules="validRule"
-             status-icon
-             class="mes-page">
-      <el-form-item label="评论" prop="content">
-        <el-input type="textarea" :rows="3" v-model="mesModel.content"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button v-if="showLogin()" type="primary" size="medium" @click="openLogin">登录发表评论
-        </el-button>
-        <el-button v-else type="primary" size="medium" @click="onSubmit" :loading="mesing">评论
-        </el-button>
-      </el-form-item>
-      <mes-board-list ref="mesBoardList"></mes-board-list>
-    </el-form>
+  <div class="mesBoard-container">
+    <mes-board-add @refresh="refresh()"></mes-board-add>
+    <mes-board-list ref="mesBoardList"></mes-board-list>
   </div>
 </template>
 
 <script>
-  import Service from '../../../../config/service'
-  import AuthUtil from '../../../../utils/authUtil'
+  import MesBoardAdd from './MesBoardAdd'
   import MesBoardList from './MesBoardList'
 
   export default {
-    name: "Mesbox",
+    name: "MesBoard",
     components: {
+      "mesBoardAdd": MesBoardAdd,
       "mesBoardList": MesBoardList
     },
-    data() {
-      return {
-        userName: '',
-        mesing: false,
-        mesModel: {
-          content: '',
-          parentId: null
-        },
-        validRule: {
-          content: [
-            {required: true, message: '请输入评论内容', trigger: 'blur'}
-          ]
-        }
-      }
-    },
     methods: {
-      onSubmit(event) {
-        event.preventDefault();
-        this.$refs.mesForm.validate((valid) => {
-          if (valid) {
-            this.mesing = true;
-            this.$axios.post(Service.url.message, this.mesModel,
-              {
-                headers: {
-                  'Authorization': localStorage.getItem('token')
-                }
-              }).then(
-              (res) => {
-                if (res.status === 200) {
-                  let responseData = res.data;
-                  if (responseData.code === 0) {
-                    this.mesing = false;
-                    this.$message({
-                      message: responseData.msg,
-                      type: 'success'
-                    });
-
-                    this.$refs.mesForm.resetFields();
-                    this.$refs.mesBoardList.getMesList();
-                  } else {
-                    this.mesing = false;
-                    this.$message.error(responseData.msg);
-                  }
-                } else {
-                  this.mesing = false;
-                  this.$message.error("系统内部错误");
-                }
-              })
-          }
-        })
-      },
-      authCheck() {
-        this.$axios.get(Service.url.authCheck, {
-          headers: {
-            'Authorization': localStorage.getItem('token')
-          }
-        }).then((res) => {
-          if (res.status === 200) {
-            let responseData = res.data;
-            let code = responseData.code;
-            if (code === -1 || code === -2) {
-              AuthUtil.clearSession();
-            } else {
-              let userName = localStorage.getItem('userName');
-              this.userName = userName;
-            }
-          } else {
-            this.$message.error("系统内部错误");
-          }
-        }).catch(function (error) {
-          console.error(error);
-        });
-      },
-      openLogin() {
-        this.$router.push('/login');
-      },
-      showLogin() {
-        if (this.userName == null || this.userName == '') {
-          return true;
-        }
-
-        return false;
-      },
-    },
-    mounted() {
-      this.authCheck();
+      refresh() {
+        this.$refs.mesBoardList.getMesList();
+      }
     }
   }
 </script>
