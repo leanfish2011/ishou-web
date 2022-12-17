b433c67eb7fade923d215989cf063fc72e1a0f11
yufei <yangtze_yufei@163.com>
2022-12-17 22:42:41 +0800

    feat: add email

--- src/components/home/item/Message.vue
+++ src/components/home/item/Message.vue
@@ -2,7 +2,7 @@
   <div id="message">
     <nav-header></nav-header>
     <div class="aboutContent">
-      <h4>你的建议，是我们前进最大的动力</h4>
+      <h4>欢迎留言，或者发送邮件：<span style="color: #2f65ca;">leanfish2011@163.com</span></h4>
       <mes-board></mes-board>
       <!-- <el-tabs>
         <el-tab-pane label="网站账号留言">
